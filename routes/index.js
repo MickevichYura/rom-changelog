dc6f3350f1edaae2ad613038bada92d41150a88f
Alexander Babai <sustem34@gmail.com>
2016-03-18 23:03:31 +0300

    parse device specific repositories (WIP)

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,13 @@ var express = require('express');
 var router = express.Router();
 var myjs = require('../other/entities');
 
+const entitiesHelper = myjs.entitiesHelper;
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
+    var config = entitiesHelper.getKernelRepository(0);
     res.render('index', {title: 'Express'});
+
 });
 
 module.exports = router;
